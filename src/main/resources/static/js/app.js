588b14f218a041aa0f46a588bf43eb5bbed32c0c
Leonardo Rocha <leonardossr@gmail.com>
2025-05-14 09:13:44 -0300

    adiciona comentários no código js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,19 +1,21 @@
+// Responsável pelo consumo via GET
 async function buscarMensagem() {
     try {
         const response = await fetch('/api/mensagem');
-        const data = await response.text();
+        const data = await response.text(); // promise
         document.getElementById('resultado-get').innerText = data;
     } catch (error) {
         alert('Erro ao buscar mensagem: ' + error);
     }
 }
 
+// Responsável pelo consumo via POST
 async function enviarDados() {
     const input = document.getElementById('dados-input').value;
     if (!input) return alert('Digite algo!');
 
     try {
-        const response = await fetch('/api/dados', {
+        const response = await fetch('/api/dados', { // promise
             method: 'POST',
             headers: {
                 'Content-Type': 'text/plain'
